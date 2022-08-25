6d3562b73478c6042d2504eca4bd9aa53a258e41
kat-5 <catalinaolend@gmail.com>
2022-08-24 18:29:38 -0700

    weather code 2

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -3,8 +3,8 @@ package class01;
 public class gitBasics {
 
     public static void main(String[] args) {
-
+         // write down a code that checks the temperature
         //blabla
-        System.out.println("today is a beautiful day");
+        System.out.println("today is not a beautiful day");
     }
 }
